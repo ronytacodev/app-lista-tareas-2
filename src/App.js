feec5be96ca20fca8371c5182af2ebcef5275e52
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-01-21 20:25:35 -0500

    agregando mas funcionalidad

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import { useState } from "react";
 import { Title } from "./components/Title";
 import { TodoInput } from "./components/TodoInput";
 import { TodoList } from "./components/TodoList";
@@ -6,17 +7,36 @@ import { Todo } from "./components/Todo";
 
 function App() {
 
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Watch the next Netflix Serie',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Read the book The Lord of Rings',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Take a shower',
+      completed: false,
+    },
+    {
+      id: 4,
+      title: 'Study 2 hours',
+      completed: false,
+    }
+
+  ])
 
   return (
     <div className="bg-gray-900 min-h-screen h-full font-inter text-gray-100 flex items-center justify-center py-20 px-5">
         <div className="container flex flex-col max-w-xl">
           <Title/>
           <TodoInput/>
-          <TodoList>
-              <Todo/>
-              <Todo/>
-              <Todo/>
-          </TodoList>
+          <TodoList todos={todos}/>
         </div>
     </div>
   );
--- src/components/TodoFilters/TodoFilters.jsx
+++ src/components/TodoFilters/TodoFilters.jsx
@@ -1,10 +1,22 @@
-import { FiltersContainer, ItemsLeft } from "./TodoFilters.components"
+import { FilterButton, FilterButtonContainer, FiltersContainer, ItemsLeft } from "./TodoFilters.components"
 
 const TodoFilters = () => {
     return (
         <FiltersContainer>
             <ItemsLeft />
+            <FilterButtonContainer>
+                <FilterButton action={() => { }} active="All" filter='All' />
+                <FilterButton action={() => { }} active="All" filter='Active' />
+                <FilterButton action={() => { }} active="All" filter='Completed' />
+            </FilterButtonContainer>
+
+            <button className="text-gray-400 hover:text-white cursor-pointer transition-all duration-300 ease-in">
+                Clear Completed
+            </button>
+
         </FiltersContainer>
+
+
     )
 }
 
